771f1bdaa640f4757bedc6998f3f286944d00b85
Neha Kumari <neha.kumari80923@gmail.com>
2025-06-26 21:51:22 +0530

    add js files

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 console.log("welcome to spotify");
 
+
+//Initalize the Variables
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar');
+
+
+
+
 let songs = [
-    {
-        songName: "salam-e-Ishq" , filePath: "Song/1.mp3" , CoverPath: "covers/1.jpg"
-    }
+    
+        {songName: "salam-e-Ishq" , filePath: "Song/1.mp3" , CoverPath: "covers/1.jpg"},
+         {songName: "salam-e-Ishq" , filePath: "Song/1.mp3" , CoverPath: "covers/1.jpg"},
+         {songName: "salam-e-Ishq" , filePath: "Song/1.mp3" , CoverPath: "covers/1.jpg"},
+         {songName: "salam-e-Ishq" , filePath: "Song/1.mp3" , CoverPath: "covers/1.jpg"},
+         { songName: "salam-e-Ishq" , filePath: "Song/1.mp3" , CoverPath: "covers/1.jpg"},
+        { songName: "salam-e-Ishq" , filePath: "Song/1.mp3" , CoverPath: "covers/1.jpg"},
+           
 ]
 
- let autdioElement = new Audio('1.mp3');
- autdioElement.play();
+
+ //autdioElement.play();
+
+// Handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime <=0){
+        audioElement.play();
+    }
+    })
+
+
+
+//  Listen to Events
+myProgressBar.addEventListener('timeupdate', ()=>{
+  console.log('timeupdate');
+  //update Seekbar
+})
