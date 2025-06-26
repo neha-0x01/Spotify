ec6b3a52bb8f924d73fd611a3891c59ae3b9525f
Neha Kumari <neha.kumari80923@gmail.com>
2025-06-26 20:34:08 +0530

    add files

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("welcome to spotify");
+
+let songs = [
+    {
+        songName: "salam-e-Ishq" , filePath: "Song/1.mp3" , CoverPath: "covers/1.jpg"
+    }
+]
+
+ let autdioElement = new Audio('1.mp3');
+ autdioElement.play();
